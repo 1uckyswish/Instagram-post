8b2d2dc8fb77a60be5b027802c58977d88cda598
Noel Guillen <noelblas274@gmail.com>
2023-08-18 19:08:52 -0400

    starter code applied

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import image1 from "../src/assets/Suit.png";
+
+function App() {
+  return (
+    <div>
+      <p>Hello</p>
+      <img src={image1} />
+    </div>
+  )
+}
+
+export default App
