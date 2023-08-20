7ee40cabe9a2d384768f474605c2316cc65b37d4
Noel Guillen <noelblas274@gmail.com>
2023-08-19 22:33:20 -0400

    array of objects for data

---
+++ src/Utils/data.js
@@ -0,0 +1,56 @@
+import Image1 from "../assets/Pfp/Image1.png"
+import Image2 from "../assets/Pfp/Image2.png"
+import Image3 from "../assets/Pfp/Image3.png"
+import Image4 from "../assets/Pfp/Image4.png"
+import Image5 from "../assets/Pfp/Image5.png"
+import Image6 from "../assets/Pfp/Image6.png"
+
+const data = [
+    {
+        Id: 1,
+        Username: "Rafaelfigini",
+        Caption: 'O ano é 2073. Tatuadores e tatuados mudam a cor e aumentam o brilho de suas tatuagens através de dispositivos futuristas.',
+        TimeStamp: '3d',
+        Like: '1 Like',
+        Image: Image3,
+        HashTags: '#IA #nftbrazil #AI #DallE',
+    },
+    {
+        Id: 2,
+        Username: 'Salvadxrx',
+        Caption: 'Server isnt working',
+        TimeStamp: '1d',
+        Like: '1 Like',
+        Image: Image4,
+        HashTags: '',
+    },
+    {
+        Id: 3,
+        Username: 'Edubarros101',
+        Caption: 'Ainda bem que não é 2077, ou essas tattoos iam ficar todas bugadas.',
+        TimeStamp: '1d',
+        Like: '1 Like',
+        Image: Image5,
+        HashTags: '',
+    },
+    {
+        Id: 4,
+        Username: 'Theactornekhiataylor',
+        Caption: 'None of my prompts look a thing like this',
+        TimeStamp: '3d',
+        Like: '5 Like',
+        Image: Image6,
+        HashTags: '',
+    },
+    //  {
+    //     Id: 5,
+    //     Username: 'Openaidalle',
+    //     Caption: 'Hungry Hungry hippos',
+    //     TimeStamp: '5d',
+    //     Like: '1 Like',
+    //     Image: Image1,
+    // },
+    
+];
+
+export default data;
