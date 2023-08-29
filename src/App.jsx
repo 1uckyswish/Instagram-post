32498dfd373beb67abe027f1de748b7646529952
Noel Guillen <noelblas274@gmail.com>
2023-08-29 18:53:22 -0400

    added starter code for 2nd portion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Post from './Components/Post/Post';
+import Homepage from './Pages/Homepage/Homepage';
 import "./App.css"
 
 function App() {
   return (
     <>
-     <Post/>
+     {/* <Post/> */}
+     <Homepage />
     </>
   )
 }
